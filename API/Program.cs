5b5bc43441f17e1b71b08eed9eaa2c9d50641087
Erkan Turhan <erkanturhan@Erkans-MacBook-Pro.local>
2022-11-27 20:54:54 +0300

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope=host.Services.CreateScope();
+            var Services=scope.ServiceProvider;
+
+            try
+            {
+                var context=Services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = Services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex,"Error during migration");
+            }
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
